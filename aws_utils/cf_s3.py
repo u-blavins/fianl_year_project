1b27e2d2d642e1a0c56e5a9d2e4677be47dda7eb
u-blavins <usama.blavins1@gmail.com>
2020-01-06 12:17:52 +0800

    Update cf_s3.py by adding a method handler in place of dictionary

--- aws_utils/cf_s3.py
+++ aws_utils/cf_s3.py
@@ -3,31 +3,28 @@ import json
 class S3_CF:
     """Class that converts s3 payload into CloudFormation"""
 
-    def __init__(self, template, payload):
+    def __init__(self, payload):
         self.payload = payload
-        self.template = template
-        self.S3_BUCKET_PROPERTIES = {
-            "AccelerationConfiguration": self.get_acceleration_config(
-                self.payload, "AccelerationConfiguration"),
-            "AccessContol": self.get_access_control(
-                self.payload, "AccessControl"),
-            "AnalyticsConfiguration": self.get_analytics_configuration(
-                self.payload, "AnalyticsConfiguration"),
-            "BucketEncryption": 0,
-            "BucketName": 0,
-            "CorsConfiguration": 0,
-            "InventoryConfigurations": 0,
-            "LifecycleConfiguration": 0,
-            "LoggingConfiguration": 0,
-            "MetricsConfigurations": 0,
-            "NotificationConfiguration": 0,
-            "ObjectLockConfiguration": 0,
-            "PublicAccessConfiguration": 0,
-            "ReplicationConfiguration": 0,
-            "Tags": 0,
-            "VersionConfiguration": 0,
-            "WebsiteCOnfigration": 0
-        }
+        self.template = {}
+        self.S3_BUCKET_PROPERTIES = [
+            "AccelerationConfiguration",
+            "AccessContol",
+            "AnalyticsConfiguration",
+            "BucketEncryption",
+            "BucketName",
+            "CorsConfiguration",
+            "InventoryConfigurations",
+            "LifecycleConfiguration",
+            "LoggingConfiguration",
+            "MetricsConfigurations",
+            "NotificationConfiguration",
+            "ObjectLockConfiguration",
+            "PublicAccessConfiguration",
+            "ReplicationConfiguration",
+            "Tags",
+            "VersionConfiguration",
+            "WebsiteCOnfigration"
+        ]
         self.construct_template()
     
     def construct_tags(self, tags):
@@ -35,58 +32,67 @@ class S3_CF:
         cf_tags = []
         for tag in tags:
             temp={}
-            temp['Key']   = tag
-            temp['Value'] = tags[tag]
+            key = list(tag.keys())[0]
+            temp['Key']   = key
+            temp['Value'] = tag[key]
             cf_tags.append(temp)
         return cf_tags
 
+    def method_handler(self, arg):
+        """ Method that handles different method calls """
+        if arg == "AccelerationConfiguration":
+            self.get_acceleration_config(arg)
+        elif arg == "AccessControl":
+            self.get_access_control(arg)
+        elif arg == "AnalyticsConfiguration":
+            self.get_analytics_configuration(arg)
 
     def construct_template(self):
         """ Method that constructs IAC template from payload """
         for bucket_property in self.payload.keys():
-            print(bucket_property)
             if bucket_property in self.S3_BUCKET_PROPERTIES:
-                self.S3_BUCKET_PROPERTIES[bucket_property]
+                self.method_handler(bucket_property)
     
-    def get_acceleration_config(self, payload, arg):
+    def get_acceleration_config(self, arg):
         """ Method that checks acceleration configuration from payload """
         accepted_options = [
             "Enabled",
             "Suspended"
         ]
-        bucket_property = payload[arg]
+        bucket_property = self.payload[arg]
         if isinstance(bucket_property, str):
             if bucket_property in accepted_options:
                 self.template[arg] = {
                     arg: bucket_property
-                }
+                    }
 
-    def get_access_control(self, payload, arg):
+    def get_access_control(self, arg):
         """ Method that checks access control configuration from payload """
         accepted_options = [
             "private", "public-read", "public-read-write",
             "aws-exec-read", "authenticated-read", "bucket-owner-read",
             "bucket-owner-full-control", "log-delivery-write"
         ]
-        bucket_property = payload[arg]
+        bucket_property = self.payload[arg]
         if isinstance(bucket_property, str):
             if bucket_property in accepted_options:
                 template[arg] = bucket_property
     
-    def get_analytics_configuration(self, payload, arg):
+    def get_analytics_configuration(self, arg):
         """ Method that checks analytics configuration from payload """
         template = {}
-        bucket_property = payload[arg]
+        bucket_property = self.payload[arg]
         if isinstance(bucket_property, dict):
             if "Id" in bucket_property:
                 template['Id'] = bucket_property['Id']
             if "Prefix" in bucket_property:
                 template['Prefix'] = bucket_property['Prefix']
             if "Destination" in bucket_property:
-                "do something here"
+                print("do something")
             if "TagFilters" in bucket_property:
                 template['TagFilters'] = self.construct_tags(
                     bucket_property['TagFilters'])
+        self.template[arg] = template
                 
 
     def get_iac_template(self):
@@ -98,6 +104,7 @@ def main():
         "AnalyticsConfiguration": {
             "Id": "test",
             "Prefix": "test",
+            "Destination": "test",
             "TagFilters": [
                 {
                     "temp": "temp"
@@ -108,8 +115,7 @@ def main():
             ]
         }
     }
-    temp = {}
-    test = S3_CF(temp, payload)
+    test = S3_CF(payload)
     print(json.dumps(test.get_iac_template(), indent=2))
     
 main()
