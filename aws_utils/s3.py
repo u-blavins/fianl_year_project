583484b174b871b926a44642713bab808b0392b2
u-blavins <usama.blavins1@gmail.com>
2020-04-13 09:57:10 +0100

    Add s3 and ses for aws services

---
+++ aws_utils/s3.py
@@ -0,0 +1,43 @@
+import boto3
+import json
+
+
+class S3:
+    """ Class for the Simple Storage Service """
+
+    def __init__(self, region='eu-west-2'):
+        """ Initialise Object """
+        self.client = boto3.client(
+            service_name='s3', region_name=region)
+
+    def backup_cloudformation_temlates(self, template, region='eu-west-2'):
+        """ Method that backs a CloudFormation Template to S3 """
+        regions = {
+            'eu-west-1': 'euw1',
+            'eu-west-2': 'euw2'
+        }
+        backup_bucket = 'cft-bucket-{}'.format(regions[region])
+        bucket_name = self.get_bucket_from_cloudformation_template(template)
+        key = '{}/{}.json'.format(region, bucket_name)
+
+        if template != {}:
+
+            response = self.client.put_object(
+                Body=json.dumps(template),
+                Bucket=backup_bucket,
+                Key=key
+            )
+
+            return response
+
+    @staticmethod
+    def get_bucket_from_cloudformation_template(template):
+        """ Method that extracts bcuekt name from a CloudFormation Template """
+        if 'Resources' in template:
+            resources = template['Resources']
+            for resource in resources.keys():
+                if resources[resource]['Type'] == 'AWS::S3::Bucket':
+                    properties = resources[resource]['Properties']
+                    if 'BucketName' in properties:
+                        return properties['BucketName']
+        return
---
+++ aws_utils/ses.py
@@ -0,0 +1,99 @@
+import boto3
+import json
+
+from s3 import S3
+
+class SES:
+    """ Class for the Simple Email Service """
+
+    def __init__(self, region='eu-west-2'):
+        """ Initialise Object """
+        self.client = boto3.client(
+            service_name='ses', region_name=region)
+        self.recipients = ['usama.blavins1@gmail.com']
+        self.source = 'infrastructure.provisioner@gmail.com'
+
+    def upload_cft_deploy_email(self, template, region):
+        """ Method that emails account owner about cft deployments """
+        bucket_name = S3.get_bucket_from_cloudformation_template(template)
+
+        email_subject = 'Deployment of {} in account'.format(bucket_name)
+        email_message = \
+            '{} has been deployed in {} through infrastructure provisioner'.format(
+                bucket_name, region)
+        message = Message()
+        message.set_subject(email_subject)
+        message.set_body('Text', email_message)
+        self.send_ses_email(message=message.get_message())
+
+    def send_ses_email(self, message):
+        """ Send an email using SES SMTP server """
+        self.client.send_email(
+            Source=self.source,
+            Destination={'ToAddresses':self.recipients},
+            Message=message
+        )
+
+    def set_source(self, source):
+        """ Setter for source email address """
+        self.source = source
+
+    def set_recipients(self, recipients):
+        """ Setter for the recipients """
+        if isinstance(recipients, list):
+            self.recipients = recipients
+
+
+class Message:
+    """ Class for creating a message """
+
+    def __init__(self):
+        self.subject = {}
+        self.body = {}
+
+    def set_subject(self, data):
+        """ Set the subject of the email """
+        if isinstance(data, str):
+            self.subject = {'Data':data}
+    
+    def get_subject(self):
+        """ Get the subject of the email """
+        return self.subject
+
+    def set_body(self, body, data):
+        """ Set the body of the email """
+        bodies = ['Text', 'Html']
+
+        if body in bodies:
+            if isinstance(data, str) and data != '':
+                self.body = {body: {'Data':data}}
+
+    def get_body(self):
+        """ Get the body of the email """
+        return self.body
+
+    def get_message(self):
+        """ Get the email message """
+        return {
+            'Subject': self.subject,
+            'Body': self.body
+        }
+
+
+
+def main():
+    ses = SES()
+    test_template = {
+        'Resources': {
+            'Bucket': {
+                'Type': 'AWS::S3::Bucket',
+                'Properties': {
+                    'BucketName': 'test-bucket-name',
+                    'Test': 'test' 
+                }
+            }
+        }
+    }
+    ses.upload_cft_deploy_email(test_template, 'eu-west-2')
+
+main()
